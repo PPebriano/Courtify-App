854f199eca2891b4312ffcdc32cc64737ea69052
PPebriano <putra.pbriano@gmail.com>
2026-08-21 14:41:08 +0700

    feat: Booking, Login, Global Exception
    Created feature inludeng booking start from IBookingService and its implementation on BookingService and also create DTOs controller. Cretaed Login request and response by AuthController, created equipment add ons endpoint for APIs

---
+++ Courtify_BE/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using CourtifyBE.DTOs;
+using CourtifyBE.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtifyBE.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            LoginResponse? result = await _authService.LoginAsync(request);
+            if (result == null) return Unauthorized(new { status = "error", message = "Email atau Password Salah" });
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Courtify_BE/DTOs/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace CourtifyBE.DTOs
+{
+    public class LoginResponse
+    {
+        public string Status { get; set; } = "success";
+        public string Token { get; set; } = string.Empty;
+        public UserSummary Admin { get; set; } = new();
+    }
+
+    public class UserSummary
+    {
+        public long Id { get; set; }
+        public string Nama { get; set; } = string.Empty;
+    }
+}
